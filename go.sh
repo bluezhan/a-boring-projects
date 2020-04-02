c15d88dd53293a8b6eda0a34892630beb1271e1d
Bruce Jenn <2252263040@qq.com>
2020-04-02 09:09:09 +0800

    chore: TIME

---
+++ go.sh
@@ -0,0 +1,10 @@
+
+TIME=("Mon Nov 30" "Mon Dec 7" "Mon Dec 14" "Tue Dec 8" "Wed Dec 9" "Thu Dec 3" "Thu Dec 10" "Thu Dec 17" "Fri Dec 11" "Fri Dec 4" "Fri Dec 18" "Wed Dec 16" "Wed Dec 2")
+
+for loop in TIME
+do
+  date >> cn.graph.info
+  git add -A
+  git commit --date="$loop 09:09:09 2020 +0800" -m "chore: $loop"
+  git push -u origin master
+done	
