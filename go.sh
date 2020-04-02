a5c838d9011bf4b82af4130e37375aeb7d6bad7d
Bruce Jenn <2252263040@qq.com>
2020-04-02 09:09:09 +0800

    chore: Thu

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 
-DAY=("Sat Jan 2" "Fri Jan 1" "Thu Dec 31" "Wed Dec 30" "Tue Dec 29" "Mon Dec 28" "Sun Dec 27")
+DAY=("Thu Dec 31" "Wed Dec 30" "Tue Dec 29" "Mon Dec 28" "Sun Dec 27")
 for loop in ${DAY[@]}
 do
   date >> cn.graph.info
