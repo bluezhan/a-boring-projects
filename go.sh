c3a691ebadf6557328faa8b9cafbc9e701d8f826
Bruce Jenn <2252263040@qq.com>
2020-11-30 09:09:09 +0800

    chore: Mon Nov 30

--- go.sh
+++ go.sh
@@ -1,9 +1,9 @@
 
 # DAY=("Thu Dec 31" "Wed Dec 30" "Tue Dec 29" "Mon Dec 28" "Sun Dec 27")
-for loop in "Sat Jan 2" "Fri Jan 1"
+for loop in "Mon Nov 30" "Mon Dec 7" "Mon Dec 14" "Tue Dec 8" "Wed Dec 9" "Thu Dec 3" "Thu Dec 10" "Thu Dec 17" "Fri Dec 11" "Fri Dec 4" "Fri Dec 18" "Wed Dec 16" "Wed Dec 2"
 do
   date >> cn.graph.info
   git add -A
-  git commit --date="$loop 09:09:09 2021 +0800" -m "chore: $loop"
+  git commit --date="$loop 09:09:09 2020 +0800" -m "chore: $loop"
   git push -u origin master
 done	
