651e9449d42da20b9b4ec42b5edbf18ca35b7ab4
Bruce Jenn <2252263040@qq.com>
2020-11-30 09:09:09 +0800

    chore: Mon Nov 30

--- go.sh
+++ go.sh
@@ -1,7 +1,5 @@
 
-TIME=("Mon Nov 30" "Mon Dec 7" "Mon Dec 14" "Tue Dec 8" "Wed Dec 9" "Thu Dec 3" "Thu Dec 10" "Thu Dec 17" "Fri Dec 11" "Fri Dec 4" "Fri Dec 18" "Wed Dec 16" "Wed Dec 2")
-
-for loop in TIME
+for loop in "Mon Nov 30" "Mon Dec 7" "Mon Dec 14" "Tue Dec 8" "Wed Dec 9" "Thu Dec 3" "Thu Dec 10" "Thu Dec 17" "Fri Dec 11" "Fri Dec 4" "Fri Dec 18" "Wed Dec 16" "Wed Dec 2"
 do
   date >> cn.graph.info
   git add -A
