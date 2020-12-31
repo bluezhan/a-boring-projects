fd8bdb5d58506fbb73bf6e0ea2b35a27b342b4f2
Bruce Jenn <2252263040@qq.com>
2020-12-31 09:09:09 +0800

    chore: Thu Dec 31

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 
-DAY=("Thu Dec 31" "Wed Dec 30" "Tue Dec 29" "Mon Dec 28" "Sun Dec 27")
-for loop in ${DAY[@]}
+# DAY=("Thu Dec 31" "Wed Dec 30" "Tue Dec 29" "Mon Dec 28" "Sun Dec 27")
+for loop in "Thu Dec 31" "Wed Dec 30" "Tue Dec 29" "Mon Dec 28" "Sun Dec 27"
 do
   date >> cn.graph.info
   git add -A
